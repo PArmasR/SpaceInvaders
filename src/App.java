a4560519a20a62e45aaae8e119dfda7bb19aedb5
PArmasR <parmasr@ulasalle.edu.pe>
2024-11-25 18:33:54 -0500

    Se agrega el Proyecto

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        //windows variables
+        int tileSize = 32;
+        int rows = 16;
+        int columns = 16;
+        int boardWidth = tileSize * columns; //32*16=512px
+        System.out.println("prueba de github");
+
+    }
+}
